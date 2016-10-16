97272db841b94b5356ca9066b6f3b16b04384f45
Reyna DeLoge <reyna.deloge@gmail.com>
2016-10-16 01:41:09 -0600

    WIP user authentication

---
+++ config/initializers/devise.rb
@@ -0,0 +1,21 @@
+Devise.setup do |config|
+
+  config.mailer_sender = 'do-not-reply@thoughtsontime.com'
+
+  require 'devise/orm/active_record'
+  config.case_insensitive_keys = [:email]
+  config.strip_whitespace_keys = [:email]
+  config.skip_session_storage = [:http_auth]
+  config.stretches = Rails.env.test? ? 1 : 10
+  config.reconfirmable = true
+  config.expire_all_remember_me_on_sign_out = true
+  config.password_length = 8..72
+  config.reset_password_within = 6.hours
+  config.sign_out_via = :delete
+
+  # ==> OmniAuth
+  config.omniauth :facebook, ENV['facebook_app_id'], ENV['facebook_app_secret'],
+    :scope => 'email', :display => 'popup',
+    :info_fields => 'first_name,last_name,email'
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   resources :recipients
-  root 'recipients#index'
+  root 'dashboard#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
